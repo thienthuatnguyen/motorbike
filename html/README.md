8ca58483d1d819f637701b92f09c2857bfb826e0
Quang Nguyen <qnguyen@dminc.com>
2019-07-28 21:35:08 +0700

    html for home page

---
+++ html/README.md
@@ -0,0 +1,16 @@
+##Demo
+http://beecow.mediastep.com
+
+##Use this if
+You need to support IE9 and below
+
+###Get Started
+
+- Open up git shell and enter the following commands
+
+    npm install
+
+    bower install
+
+    grunt watch
+
---
+++ html/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    if ($('.regular').length) {
+      $(".regular").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1140,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+            }
+          }
+        ]
+  
+      });
+    }
+  
+    if ($('.slider-header').length) {
+      $('.slider-header').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+    }
+  });
